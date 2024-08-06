ca24bf0d8e4efb659cfbcfcd58dbaf34752eff21
Serxho Cungeli <serxho_cungeli@unishk.edu.al>
2024-08-06 13:15:42 +0200

    update and delete

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './routes/user.route.js';
 import authRoute from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ mongoose.connect(process.env.MONGO)
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute);
