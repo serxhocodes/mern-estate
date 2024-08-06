4072c178612681ea5d49ccc8e7252bd060f592b6
Serxho Cungeli <serxho_cungeli@unishk.edu.al>
2024-08-06 14:14:04 +0200

    finish with delete acc

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, test, updateUser } from '../controller/user.controller.js';
+import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
