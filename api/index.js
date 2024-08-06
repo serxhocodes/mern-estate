97360d6f616ea400c7ec94b472f483a418a0f940
Serxho Cungeli <serxho_cungeli@unishk.edu.al>
2024-08-06 20:48:25 +0200

    pre installation and fix

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoute from './routes/user.route.js';
 import authRoute from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO)
   .catch((err) => {
     console.log(err);
   });
+  const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -23,6 +25,13 @@ app.use(cookieParser());
 app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute);
 app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = res.statusCode || 500 ;
